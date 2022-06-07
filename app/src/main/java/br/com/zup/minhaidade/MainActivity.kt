f396166e7aa58e3d03d0c1dc1e7713f0497f875d
Sabrina Porto Mota de Souza <sabrinapmota@yahoo.com.br>
2022-06-07 18:14:46 -0300

    adicionada a condição na idade

--- app/src/main/java/br/com/zup/minhaidade/MainActivity.kt
+++ app/src/main/java/br/com/zup/minhaidade/MainActivity.kt
@@ -3,7 +3,7 @@ package br.com.zup.minhaidade
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import br.com.zup.minhaidade.databinding.ActivityMainBinding
-    
+
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -12,5 +12,19 @@ class MainActivity : AppCompatActivity() {
             layoutInflater
         )
         setContentView(binding.root)
+        binding.btValidacao.setOnClickListener {
+            val nome = binding.etUserName.text.toString()
+            val idade = binding.etUserAge.text.toString().toInt()
+            validarIdade(idade)
+
+        }
+    }
+
+    private fun validarIdade(idade: Int) {
+        if (idade < 18) {
+            binding.tvMessage.text = "menor de idade!"
+        } else {
+            binding.tvMessage.text = "Maior de 18!"
+        }
     }
 }
