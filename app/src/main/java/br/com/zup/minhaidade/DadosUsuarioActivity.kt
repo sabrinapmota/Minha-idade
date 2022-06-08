fd4781278fbade8f2942207bcf4e3ada6ff755ea
Sabrina Porto Mota de Souza <sabrinapmota@yahoo.com.br>
2022-06-08 17:46:15 -0300

    Adicionado activity para exibir dados do usuário

---
+++ app/src/main/java/br/com/zup/minhaidade/DadosUsuarioActivity.kt
@@ -0,0 +1,11 @@
+package br.com.zup.minhaidade
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class DadosUsuarioActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_dados_usuario)
+    }
+}
